c9d49d76b68b7150526bd6a45c7bee2b33ca652f
Nazmiassofa <nazmiassofa@gmail.com>
2026-01-05 00:21:41 +0700

    delete instagrapi

--- core/redis.py
+++ core/redis.py
@@ -16,7 +16,7 @@ async def init_redis() -> Redis:
             host=config.REDIS_HOST,
             port=config.REDIS_PORT,
             password=config.REDIS_PASSWORD,
-            ssl=True,
+            ssl=False,
             decode_responses=False,  # Keep as bytes for binary data
             socket_connect_timeout=5,
             socket_timeout=5
